b74f61718d6c2703a93d537cd8c2cdfa626cfd71
tnlvof <tnlvof2@naver.com>
2024-06-27 16:17:46 +0900

    chore: Refactor person page to handle optional data properties

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,24 +9,24 @@ const Page = async () => {
     <div className="container mx-auto mb-10">
       <div className="grid grid-cols-4">
         {data?.map((billionaire: any) => (
-          <Link href={`/person/${billionaire.id}`} key={billionaire.id}>
+          <Link href={`/person/${billionaire?.id}`} key={billionaire?.id}>
             <div key={billionaire.id} className="mb-6">
               <Image
                 className="rounded-sm"
                 src={billionaire?.squareImage}
                 width={250}
                 height={250}
-                alt={billionaire.name}
+                alt={billionaire?.name}
                 unoptimized
               />
-              <h2 className="text-base font-bold">{billionaire.name}</h2>
+              <h2 className="text-base font-bold">{billionaire?.name}</h2>
               <p>
                 <span className="text-sm text-gray-700 font-medium">
-                  {convertMillionToBillion(billionaire.netWorth)} Billion
+                  {convertMillionToBillion(billionaire?.netWorth)} Billion
                 </span>{" "}
                 /{" "}
                 <span className="text-sm text-gray-700">
-                  {billionaire.industries}
+                  {billionaire?.industries}
                 </span>
               </p>
             </div>
--- src/app/person/[id]/page.tsx
+++ src/app/person/[id]/page.tsx
@@ -40,57 +40,61 @@ export default async function Page({ params }: { params: { id: string } }) {
         <CardContent>
           <div className="grid gap-8">
             <div>
-              <p className="text-xl font-bold">{data.name}</p>
+              <p className="text-xl font-bold">{data?.name}</p>
             </div>
             <div className="text-lg font-medium">
-              <p>State : {data.state}</p>
-              <p>City : {data.city}</p>
-              <p>Country : {data.country}</p>
-              <p>Industries : {data.industries}</p>
-              <p>Rank : {data.position}</p>
-              <p>NetWorth : {convertMillionToBillion(data.netWorth)} Billion</p>
+              <p>State : {data?.state}</p>
+              <p>City : {data?.city}</p>
+              <p>Country : {data?.country}</p>
+              <p>Industries : {data?.industries}</p>
+              <p>Rank : {data?.position}</p>
+              <p>
+                NetWorth : {convertMillionToBillion(data?.netWorth)} Billion
+              </p>
             </div>
             <div>
-              <p>{data.about}</p>
+              <p>{data?.about}</p>
             </div>
             <div>
-              {data.bio.map((bio: string, index: number) => (
+              {data?.bio?.map((bio: string, index: number) => (
                 <li key={index}>{bio}</li>
               ))}
             </div>
           </div>
         </CardContent>
-        <CardFooter>
-          <div className="w-full mt-10">
-            <p className="text-lg font-medium">FinancialAssets</p>
-            <Table>
-              <TableHeader>
-                <TableRow>
-                  <TableHead>Exchange</TableHead>
-                  <TableHead>Ticker</TableHead>
-                  <TableHead>Company</TableHead>
-                  <TableHead>Shares</TableHead>
-                  <TableHead>SharePrice</TableHead>
-                </TableRow>
-              </TableHeader>
-              <TableBody>
-                {data.financialAssets.map((asset: any, index: number) => (
-                  <TableRow key={index}>
-                    <TableCell> {asset.exchange}</TableCell>
-                    <TableCell>{asset.ticker}</TableCell>
-                    <TableCell>{asset.companyName}</TableCell>
-                    <TableCell>
-                      {asset.numberOfShares.toLocaleString()}
-                    </TableCell>
-                    <TableCell>
-                      {asset.sharePrice.toLocaleString()} {asset.currencyCode}
-                    </TableCell>
+        {data.financialAssets && (
+          <CardFooter>
+            <div className="w-full mt-10">
+              <p className="text-lg font-medium">FinancialAssets</p>
+              <Table>
+                <TableHeader>
+                  <TableRow>
+                    <TableHead>Exchange</TableHead>
+                    <TableHead>Ticker</TableHead>
+                    <TableHead>Company</TableHead>
+                    <TableHead>Shares</TableHead>
+                    <TableHead>SharePrice</TableHead>
                   </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </div>
-        </CardFooter>
+                </TableHeader>
+                <TableBody>
+                  {data?.financialAssets?.map((asset: any, index: number) => (
+                    <TableRow key={index}>
+                      <TableCell> {asset.exchange}</TableCell>
+                      <TableCell>{asset.ticker}</TableCell>
+                      <TableCell>{asset.companyName}</TableCell>
+                      <TableCell>
+                        {asset.numberOfShares.toLocaleString()}
+                      </TableCell>
+                      <TableCell>
+                        {asset.sharePrice.toLocaleString()} {asset.currencyCode}
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </div>
+          </CardFooter>
+        )}
       </Card>
     </>
   );
